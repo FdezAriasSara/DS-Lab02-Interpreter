1e6b5d9d8b168ea7970da526203b812a87dc0c72
Sara Fernández Arias <uo269546@uniovi.es>
2022-09-22 12:58:54 +0200

    class work

---
+++ fernandez_arias_sara_practica2/src/instructions/Instruction.java
@@ -0,0 +1,5 @@
+package instructions;
+import app.Interpreter;
+public interface Instruction {
+	public void execute(Interpreter interpreter);
+}
---
+++ fernandez_arias_sara_practica2/src/instructions/Push.java
@@ -0,0 +1,12 @@
+package instructions;
+
+import app.Interpreter;
+
+public class Push implements Instruction {
+
+	@Override
+	public void execute(Interpreter interpreter) {
+		//TODO
+	}
+
+}
